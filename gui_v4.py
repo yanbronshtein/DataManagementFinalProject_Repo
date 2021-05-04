fcb81b4e308aeb27eff12698a26de72ac7a75113
Yaniv Bronshtein <yanbronshtein@gmail.com>
2021-05-04 12:41:16 -0400

    Finished documentation for both gui v4 and crud v2

--- gui_v4.py
+++ gui_v4.py
@@ -1,15 +1,17 @@
+# Drivers for the 3 databases
 import pymongo
 import mysql.connector
-import pandas as pd
+import redis
+
+import sys
+from dotenv import dotenv_values
+from datetime import timedelta
+import datetime
 import sys
 from dotenv import dotenv_values
 from tkinter import *
-from tkinter import ttk
 from crud_v2 import CRUD
 import os
-import datetime
-##VIP global variables
-
 
 query_option_list = ['Search by Hashtag', 'Search by Word', 'Search by User Screen Name', 'Search by Time Range']
 button_list = []
@@ -17,130 +19,85 @@ button_list = []
 
 class GUI:
     def __init__(self, root):
+        """
+        This initializes the GUI application including the radio buttons for user query type, user entry box
+        and the quit, go, and clear output buttons, and the output summary label
+        :param root:
+        """
         self.root = root
         self.root.title('Tweet Search Application')
-        # self.root.geometry('1000x1000')
-        self.root.attributes("-fullscreen", True)
-        self.search_choice = IntVar()
-        self.user_query = StringVar()
-        self.welcome()
-        self.elapsed_time_ms = ''
-        for i in range(len(query_option_list)):
-            # button = Radiobutton(root, text=query_option_list[i], variable=search_choice, value=i+1, command=sel)
-            button = Radiobutton(self.root, text=query_option_list[i], variable=self.search_choice, value=i + 1)
+        self.root.attributes("-fullscreen", True)  # Make full screen
+        self.search_choice = IntVar()  # Global variable to save the radio button choice. One of {1,2,3,4}
+        self.user_query = StringVar()  # Global variable to save the query typed by user in entry
+        self.welcome()  # Create welcome label
 
-            # button_list.append(button)
+        # Initialize the radio buttons
+        for i in range(len(query_option_list)):
+            button = Radiobutton(self.root, text=query_option_list[i], variable=self.search_choice, value=i + 1)
             button.pack(anchor=W)
 
+        # Initialize the entry box
         self.entry = Entry(self.root)
         self.entry.pack()
+        # Initialize the go button with the go() callback function
         go_button = Button(self.root, text="Go", command=self.go)
         go_button.pack()
-
+        # Initialize the quit button with the command to destroy the frame and quit the search application
         quit_button = Button(self.root, text="Quit", command=self.root.destroy)
         quit_button.pack()
+
+        # Initialize the clear output button with the clear() callback function
         clear_output_button = Button(self.root, text="Clear output", command=self.clear)
         clear_output_button.pack()
 
-        #Labels
-        # userinfo_label = Label(self.root, text="User Info\n")
-        # userinfo_label.pack(side=LEFT)
-        # tweetinfo_label = Label(self.root, text="Tweet Info\n")
-        # tweetinfo_label.pack(side=LEFT)
-        # tweetinfo_label.pack(side=RIGHT)
-
-        #Tweet Info
-
-        self.sb1 = Scrollbar(self.root)
-        # self.sb1.pack(side=RIGHT, fill=Y)
-        # self.tweetinfo_guiList = Listbox(self.root, yscrollcommand=self.sb1.set, width=150)
-        self.tweetinfo_guiList = Listbox(self.root, yscrollcommand=self.sb1.set, bg="yellow")
-        # self.tweetinfo_guiList.pack(side=LEFT)
-        self.sb1.config(command=self.tweetinfo_guiList.yview)
-
-        #User info
-        self.sb2 = Scrollbar(self.root)
-        # self.sb2.pack(side=RIGHT, fill=Y)
-        # self.userinfo_gui_list = Listbox(self.root, yscrollcommand=self.sb2.set, width=150)
-        self.userinfo_gui_list = Listbox(self.root, yscrollcommand=self.sb2.set)
-
-        # self.userinfo_gui_list.pack(side=LEFT)
-        self.sb2.config(command=self.userinfo_gui_list.yview)
-
-
     def welcome(self):
+        """
+        This function simply renders the welcome label. This is the first thing the user sees when opening
+        the application
+        :return: None
+        """
         welcome_str = """
             Welcome to the Tweet Search Application! 
             Please choose a search option, the requested field and your query
-            Example Choice 1: Radio button: "Search by Hashtag", Entry: "sundayvibes", Then Click "Go"
+            Example Choice 1: Radio button: "Search by Hashtag", Entry: "SundayVibes", Then Click "Go"
             
-            Example Choice 2: Radio button: "Search by Word", Entry: "Evil can not spread without followers.", Then Click "Go"
+            Example Choice 2: Radio button: "Search by Word", Entry: "Evil", Then Click "Go"
             
-            Example Choice 3: Radio button: "Search by User", Entry: "SistaAkos", Then Click "Go"
+            Example Choice 3: Radio button: "Search by User", Entry: "GenYtakeover", Then Click "Go"
             
-            Example Choice 4: Radio button: "Search by Time Range", 
-            Entry: "2018-06-29 17:08:00,2020-06-29 17:08:00" where the first value is the start date
-            and the second value is the end date in UTC datetime. Make sure to use a comma as a separator Then Click "Go"
-            """
+            Example Choice 4: Radio button: "Search by Time Range", Entry: "2021-04-11 19:32:20,2021-04-11 19:32:50" 
+            where the first value is the start date and the second value is the end date in UTC datetime. Make sure 
+            to use a comma as a separator Then Click "Go" """
         welcome_label = Label(self.root, text=welcome_str, font=("Arial", 18), fg='blue')
         welcome_label.pack()
 
     def clear(self):
+        """
+        This function destroys the application and then restarts the application with a call to main()
+        :return: None
+        """
         self.root.destroy()
         main()
 
-    # def separator(self):
-    #     separator = ttk.Separator(root, orient='horizontal')
-    #     separator.pack(fill='x')
-
-    # def merge_dicts(self, dict1, dict2):
-    #     new_dict = {**dict1, **dict2}
-    #     return new_dict
-
     def go(self):
-
-        print("User search choice", self.search_choice.get())
+        """
+        This function works when the user selects a radio button and types in text into the entry
+        The results are obtained from the global variables. An object of the CRUD class in instantiated to help
+        with the backend of the application.
+        Ultimately, the results of one of the four methods in CRUD methods is displayed in an output label
+        :return: None
+        """
         choice = self.search_choice.get()
         user_text = self.entry.get().strip()
-        # print("User text was", user_text)
         crud = CRUD()
-
-
-        global user_info_list
-        global tweet_info_list
-        user_info_list = []  # Store SQL user information
-        tweet_info_list = [] # Store MongoDB tweet information
-
         res = ""
         if choice == 1:
-
             res = crud.search_by_hashtag(user_text)
-
         elif choice == 2:
             res = crud.search_by_word(user_text)
         elif choice == 3:
             res = crud.search_by_user(user_text)
-
-            # print("choice was to search by user")
-            # sql_query = """
-            # SELECT * FROM user WHERE screen_name='{}';
-            # """.format(user_text)
-            # sql_res = crud.get_mysql(sql_query)
-            # # if len(sql_res) == 0:
-            # #     print("The screen_name", user_text, "does not exist")
-            #
-            # for record in sql_res:
-            #     user_info_list.append(str(record) + '\n')
-            #     mongo_query = {'mongo_tweet_id': record['sql_tweet_id'], 'mongo_user_id': record['sql_user_id']}
-            #     mongo_res = crud.get_mongo(mongo_query)
-            #     for doc in mongo_res:
-            #         doc_dict = dict(doc)  # Get the dictionary version of this
-            #
-            #         tweet_info_list.append(str(doc_dict) + '\n')
-
-        # by time range
         elif choice == 4:
-            # print("choice was to search by time range")
             try:
                 lower_bound, upper_bound = user_text.split(',')  # Get start and end dates
                 lower_bound = datetime.datetime.strptime(lower_bound.strip(), '%Y-%m-%d %H:%M:%S')
@@ -149,97 +106,23 @@ class GUI:
                 res = crud.search_by_time_range(lower_bound, upper_bound)
 
             except ValueError:
-                res = """ERROR: the query by user <{}> threw an error because two comma separated values were not provided
-                            Please clear the output and try again""".format(user_text)
+                res = """ERROR: the query by user <{}> threw an error because two comma separated values were not 
+                provided Please clear the output and try again""".format(user_text)
 
-            # start_timestamp = crud.make_timestamp(start_date)
-            # end_timestamp = crud.make_timestamp(start_date)
-            # mongo_query = {"created_date": {"$gte": start_timestamp, "$lt": end_timestamp}}
-            # mongo_res = crud.get_mongo(mongo_query)
-            # for doc in mongo_res:
-            #     doc_dict = dict(doc)  # Get the dictionary version of this
-            #     tweet_info_list.append(str(doc_dict))
-            #     sql_query = """
-            #             SELECT * FROM user WHERE sql_tweet_id = '{}' and sql_user_id = '{}';
-            #             """.format(doc_dict['mongo_tweet_id'], doc_dict['mongo_user_id'])
-            #     sql_res = crud.get_mysql(sql_query)
-            #     # the composite key of user_id and tweet_id is unique in SQL so merge_dicts() will work
-            #     for record in sql_res:
-            #         user_info_list.append(str(record) + '\n')
-
-        print("res yo", res)
         bg = 'red' if 'ERROR' in res else 'yellow'
-        # # print(res[0])
-        # print(bg)
-        self.summary_label = Label(self.root, bg=bg, width=300, text=res)
-        self.summary_label.pack()
 
-        # sql_res = crud.get_mysql(sql_query)
-        #
-        #
-        # print("Type SQLres", type(sql_res))
-        # print("Type mongores", type(mongo_res))
-
-        # # print(res)
-####################################################################
-        # self.tweetinfo_guiList = Listbox(self.root, yscrollcommand=self.sb1.set, bg="yellow", width=75)
-        # self.sb1.config(command=self.tweetinfo_guiList.yview)
-        # for row in tweet_info_list:
-        #     self.tweetinfo_guiList.insert(END, str(row))
-        # 
-        # self.tweetinfo_guiList.pack(side=LEFT)
-        # self.sb1.pack(side=LEFT, fill=Y)
-        # self.sb1.config(command=self.tweetinfo_guiList.yview)
-        # 
-        # 
-        # # User info
-        # self.sb2 = Scrollbar(self.root)
-        # self.userinfo_gui_list = Listbox(self.root, yscrollcommand=self.sb2.set, bg="light blue", width=75)
-        # 
-        # # self.userinfo_gui_list.pack(side=LEFT)
-        # self.sb2.config(command=self.userinfo_gui_list.yview)
-        # for row in user_info_list:
-        #     self.userinfo_gui_list.insert(END, str(row))
-        # 
-        # self.userinfo_gui_list.pack(side=LEFT)
-        # self.sb2.pack(side=LEFT, fill=Y)
-
-        # self.sb2.config(command=self.userinfo_gui_list.yview)
-#####################################################################
+        summary_label = Label(self.root, bg=bg, width=300, text=res)
+        summary_label.pack()
 
 
-
-
-
-    # root.update_idletasks()
-    #
-
-
-# def scrollbar(root):
-#     sb = Scrollbar(root)
-#     sb.pack(side=RIGHT, fill=Y)
-#     crud = CRUD()
-#     mongo_query = {'hashtags': {'$elemMatch': {'$eq': 'sundayvibes'}}}
-#     # res = list(crud.get_mongo(query=mongo_query))
-#     res = crud.get_mongo(mongo_query)
-#
-#     mylist = Listbox(root, yscrollcommand=sb.set, width=300)
-#     # print(res)
-#     for doc in res:
-#         mylist.insert(END, str(dict(doc)))
-#
-#     mylist.pack(side=LEFT)
-#     sb.config(command=mylist.yview)
-
-
-# # print(res)
-
-
-# mylist.pack(side=LEFT)
-# sb.config(command=mylist.yview)
 def main():
+    """
+    This function is called as soon as someone executes this python file.
+    The GUI is initialized and started
+    :return: None
+    """
     root = Tk()
-    app = GUI(root)
+    GUI(root)
     root.mainloop()
 
 
